e2856119db51e45d052822269590bc6e5bd9a38b
My Tran <mytran1109@gmail.com>
2021-10-14 16:15:01 +0700

    Add files via upload

---
+++ SAAS_REPO_CLIENT/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { SubscriptionItemDetailComponent } from './components/subscription-item-detail/subscription-item-detail.component';
+import { AboutComponent } from './components/about/about.component';
+import { AddSubscriptionItemComponent } from './components/add-subscription-item/add-subscription-item.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+  { path: 'home', component: WelcomeComponent },
+  // { path: 'addSubscription', component: AddSubscriptionItemComponent },
+  { path: 'addSubscription/:userId/:listId', component: AddSubscriptionItemComponent },
+  { path: 'subscriptions/:userId', component: SubscriptionListComponent },
+  { path: 'subscription/:userId/:itemId', component: SubscriptionItemDetailComponent, },
+  { path: 'about', component: AboutComponent},
+  { path: 'profile/:userId', component: ProfileComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
